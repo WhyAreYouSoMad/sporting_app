f26ae418b5ed10b0bc912b8ff860d9cf628e14cd
Jeonkukil <a0211a@naver.com>
2023-04-12 10:58:06 +0900

    mainpage

---
+++ lib/view/pages/sport_list/golf_list.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GolfList extends StatelessWidget {
+  const GolfList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child:Text("리스트가 들어올자리"),
+      ),
+    );
+  }
+}
