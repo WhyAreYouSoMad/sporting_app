29aff4bd0060d37fefb797a7e0b5bd077f124a40
NomadHuns <kimalss22@gmail.com>
2023-04-13 10:01:38 +0900

    GFP-45 경기장 리스트 페이지 구현

--- lib/view/pages/main/componunts/recommend_stadium_tile.dart
+++ lib/view/pages/main/componunts/recommend_stadium_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sporting_app/view/componunts/rating_star.dart';
 
 class RecommendStadiumTile extends StatelessWidget {
   final String image;
@@ -6,7 +7,15 @@ class RecommendStadiumTile extends StatelessWidget {
   final double rating;
   final int reviewCount;
   final String price;
-  const RecommendStadiumTile({required this.image, required this.stadiumName, required this.price, this.rating = 0, this.reviewCount = 0, Key? key}) : super(key: key);
+
+  const RecommendStadiumTile(
+      {required this.image,
+      required this.stadiumName,
+      required this.price,
+      this.rating = 0,
+      this.reviewCount = 0,
+      Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +30,7 @@ class RecommendStadiumTile extends StatelessWidget {
             const Spacer(),
             _buildStadiumName(),
             const Spacer(),
-            _buildRatingStar(),
+            RatingStar(rating: rating, reviewCount: reviewCount),
             const SizedBox(height: 8),
             _buildRatingNum()
           ],
@@ -32,67 +41,33 @@ class RecommendStadiumTile extends StatelessWidget {
 
   Row _buildRatingNum() {
     return Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              const SizedBox(width: 100),
-              Text(
-                price,
-                style: const TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-              ),
-              const Text("원")
-            ],
-          );
-  }
-
-  Row _buildRatingStar() {
-    return Row(
-            children: [
-              Icon(
-                Icons.star,
-                color: rating >= 1 ? Colors.yellow : Colors.grey,
-                size: 14,
-              ),
-              Icon(
-                Icons.star,
-                color: rating >= 2 ? Colors.yellow : Colors.grey,
-                size: 14,
-              ),
-              Icon(
-                Icons.star,
-                color: rating >= 3 ? Colors.yellow : Colors.grey,
-                size: 14,
-              ),
-              Icon(
-                Icons.star,
-                color: rating >= 4 ? Colors.yellow : Colors.grey,
-                size: 14,
-              ),
-              Icon(
-                Icons.star,
-                color: rating >= 5 ? Colors.yellow : Colors.grey,
-                size: 14,
-              ),
-              Text(rating.toString()),
-              Text("($reviewCount)"),
-            ],
-          );
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        const SizedBox(width: 100),
+        Text(
+          price,
+          style: const TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+        ),
+        const Text("원")
+      ],
+    );
   }
 
   Text _buildStadiumName() {
     return Text(
-            stadiumName,
-            style: const TextStyle(fontWeight: FontWeight.bold),
-          );
+      stadiumName,
+      style: const TextStyle(fontWeight: FontWeight.bold),
+    );
   }
 
   ClipRRect _buildImage() {
     return ClipRRect(
-            borderRadius: BorderRadius.circular(5.0),
-            child: SizedBox(
-              width: 170,
-              height: 110,
-              child: Image.asset(image, fit: BoxFit.cover),
-            ),
-          );
+      borderRadius: BorderRadius.circular(5.0),
+      child: SizedBox(
+        width: 170,
+        height: 110,
+        child: Image.asset(image, fit: BoxFit.cover),
+      ),
+    );
   }
 }
