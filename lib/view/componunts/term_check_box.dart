e9ed16e7bfc5a90d84ab7931967a73d0bd0ff78f
NomadHuns <kimalss22@gmail.com>
2023-04-13 14:34:07 +0900

    GFP-70 경기장 등록 페이지 구현

---
+++ lib/view/componunts/term_check_box.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../common/constants.dart';
+
+class TermCheckBox extends StatelessWidget {
+  final String text;
+
+  const TermCheckBox({required this.text, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(
+          value: true,
+          onChanged: (bool? value) {},
+        ),
+        Text(text, style: const TextStyle(color: kTextColor)),
+      ],
+    );
+  }
+}
--- lib/view/pages/stadiumlist/stadium_list.dart
+++ lib/view/pages/stadiumlist/stadium_list.dart
@@ -16,24 +16,22 @@ class StadiumList extends StatelessWidget {
         width: double.infinity,
         color: kPrimaryColor,
         child: ListView(
-          children: [
-            const StadiumListCard(
-              image: "assets/billiardhall.jpg",
-              price: "60,000",
-              stadiumName: "테니스장",
-            ),const StadiumListCard(
-              image: "assets/billiardhall.jpg",
-              price: "60,000",
-              stadiumName: "테니스장",
-            ),const StadiumListCard(
+          children: const [
+            StadiumListCard(
               image: "assets/billiardhall.jpg",
               price: "60,000",
               stadiumName: "테니스장",
             ),
-            _buildStadiumCard(context),
-            _buildStadiumCard(context),
-            _buildStadiumCard(context),
-            _buildStadiumCard(context),
+            StadiumListCard(
+              image: "assets/billiardhall.jpg",
+              price: "60,000",
+              stadiumName: "테니스장",
+            ),
+            StadiumListCard(
+              image: "assets/billiardhall.jpg",
+              price: "60,000",
+              stadiumName: "테니스장",
+            ),
           ],
         ),
       ),
@@ -59,67 +57,6 @@ class StadiumList extends StatelessWidget {
           icon: const Icon(Icons.home, color: kDarkIconColor),
         )
       ],
-    );
-  }
-
-  Padding _buildStadiumCard(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        height: 260,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: kWhiteColor,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 160,
-              width: double.infinity,
-              child: ClipRRect(
-                borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10)),
-                child:
-                    Image.asset("assets/billiardhall.jpg", fit: BoxFit.cover),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10, left: 10),
-              child: Text("테니스장",
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleLarge
-                      ?.copyWith(color: kTextColor)),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: RatingStar(rating: 3, reviewCount: 300),
-            ),
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text("1시간 | ",
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleMedium
-                          ?.copyWith(color: kTextColor.withOpacity(0.4))),
-                  Text("60,000원~",
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleLarge
-                          ?.copyWith(color: kTextColor)),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
