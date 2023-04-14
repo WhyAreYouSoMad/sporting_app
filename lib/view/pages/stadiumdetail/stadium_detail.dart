9d5d99d088704f7e2d4d190c8f531686d944b8ca
NomadHuns <118786401+NomadHuns@users.noreply.github.com>
2023-04-14 19:50:09 +0900

    경기장 예약 리스트 구현 (#15)
    Co-authored-by: Jeonkukil <a0211a@naver.com>

---
+++ lib/view/pages/stadiumdetail/stadium_detail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StadiumDetail extends StatelessWidget {
+  const StadiumDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
