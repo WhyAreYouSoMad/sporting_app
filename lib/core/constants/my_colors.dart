b224904ef50b966ec7dd2f9fdd66be38aafbabc1
NomadHuns <kimalss22@gmail.com>
2023-04-21 09:03:10 +0900

    GFP-68 코어 패키지 구현

---
+++ lib/core/constants/my_colors.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+const kWhiteColor = Color(0xFFFFFFFF);
+const kBlackColor = Color(0xFF000000);
+const kTextColor = Color(0xFF1D150B);
+const kPrimaryColor = Color(0xFF2D9C36);
+const kSecondaryColor = Color(0xFFF5E1CB);
+const kBoarderColor = Color(0xFFDDDDDD);
+const kLogoColor = Color(0xFF000000);
+const kDarkIconColor = Color(0xFF000000);
+const kWhiteIconColor = Color(0xFFFFFFFF);
+const kBluewColor = Color(0xFF0000FF);
+const kGrayColor = Color(0xFFCCCCCC);
