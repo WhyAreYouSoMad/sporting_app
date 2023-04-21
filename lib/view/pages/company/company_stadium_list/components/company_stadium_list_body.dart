05ed916dc4aee2e0bbe89949937eaba976434a23
NomadHuns <kimalss22@gmail.com>
2023-04-21 14:38:13 +0900

    GFP-103 기업 회원 경기장 관리 리스트 페이지 구현

---
+++ lib/view/pages/company/company_stadium_list/components/company_stadium_list_body.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:sporting_app/view/components/my_stadium_item.dart';
+
+class CompanyStadiumListBody extends StatelessWidget {
+  const CompanyStadiumListBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+                (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(5),
+                child: MyStadiumItem(
+                  price: "2,200,000",
+                  stadiumName: "사직 야구장",
+                  location: "광안리해수욕장 도보 10분",
+                  stadiumPic: "assets/images/stadiums/baseballpark.jpg",
+                  hasEvent: false,
+                  iscard: true,
+                  hasSticky: false,
+                  onTab: () {
+                  },
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
