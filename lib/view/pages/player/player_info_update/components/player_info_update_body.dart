3b36570ada9e20f7837cce7ad40522909a1492c5
NomadHuns <kimalss22@gmail.com>
2023-04-25 18:53:28 +0900

    GFP-68 플레이어 정보 및 업데이트 페이지 경로 수정

---
+++ lib/view/pages/player/player_info_update/components/player_info_update_body.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sporting_app/view/pages/company/company_info/company_info_udate/company_info_update_contents.dart';
+
+class PlayerInfoUpdateBody extends StatefulWidget {
+   PlayerInfoUpdateBody({Key? key}) : super(key: key);
+
+  @override
+  State<PlayerInfoUpdateBody> createState() => _PlayerInfoUpdateBodyState();
+}
+
+class _PlayerInfoUpdateBodyState extends State<PlayerInfoUpdateBody> {
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Column(
+         children: [
+           MyImage(),
+           MyName(),
+           Divider(thickness: 10, height: 30),
+           _Text(),
+           Divider(thickness: 1, height: 30, color: Colors.black54),
+           BottomContent(),
+         ],
+        )
+      ],
+    );
+  }
+}
+
+
+Widget _Text() {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 20),
+    child: Row(
+      children: [
+        Text("개인정보", textAlign: TextAlign.start),
+      ],
+    ),
+  );
+}
