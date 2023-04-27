1488d6f825bfcdc5654aadfd4a95dcfba12a110b
NomadHuns <kimalss22@gmail.com>
2023-04-27 12:09:23 +0900

    GFP-14 일반 회원 정보 불러오기 Repository 테스트 완료

---
+++ lib/model/player/player_info.dart
@@ -0,0 +1,38 @@
+
+class PlayerInfo {
+  final int id;
+  final String tel;
+  final String gender;
+  final String age;
+  final String address;
+
+  PlayerInfo({
+    required this.id,
+    required this.tel,
+    required this.gender,
+    required this.age,
+    required this.address,
+  });
+
+  // 통신을 위해서 json 처럼 생긴 문자열 {"id":1} => Dart 오브젝트
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['tel'] = tel;
+    data['gender'] = gender;
+    data['age'] = age;
+    data['address'] = address;
+    return data;
+  }
+
+  factory PlayerInfo.fromJson(Map<String, dynamic> json) {
+    return PlayerInfo(
+      id: json['id'],
+      tel: json['tel'],
+      gender: json['gender'],
+      age: json['age'],
+      address: json['address'],
+    );
+  }
+}
---
+++ lib/model/user/user.dart
@@ -0,0 +1,42 @@
+
+import 'package:sporting_app/model/player/player_favorite_sport.dart';
+import 'package:sporting_app/model/player/player_info.dart';
+
+class User {
+  final int id;
+  final String nickname;
+  final String email;
+  final PlayerInfo playerInfo;
+  final PlayerFavoriteSport playerFavoriteSport;
+
+
+  User({
+    required this.id,
+    required this.nickname,
+    required this.email,
+    required this.playerInfo,
+    required this.playerFavoriteSport,
+  });
+
+  // 통신을 위해서 json 처럼 생긴 문자열 {"id":1} => Dart 오브젝트
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['nickname'] = nickname;
+    data['email'] = email;
+    data['playerInfo'] = playerInfo;
+    data['playerFavoriteSport'] = playerFavoriteSport;
+    return data;
+  }
+
+  factory User.fromJsonForPlayer(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      nickname: json['nickname'],
+      email: json['email'],
+      playerInfo: PlayerInfo.fromJson(json['playerInfo']),
+      playerFavoriteSport: PlayerFavoriteSport.fromJson(json['playerFavoriteSport']),
+    );
+  }
+}
