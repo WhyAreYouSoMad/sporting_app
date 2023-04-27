51d947b5f177f0bec5a42ea63b420da33d8f4a7c
NomadHuns <kimalss22@gmail.com>
2023-04-27 12:23:48 +0900

    GFP-14 회원 정보 수정 페이지 앱바 추가

--- lib/view/pages/player/player_info_update/player_info_update_page.dart
+++ lib/view/pages/player/player_info_update/player_info_update_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sporting_app/core/constants/my_colors.dart';
 import 'package:sporting_app/view/pages/player/player_info_update/components/player_info_update_body.dart';
 
 class PlayerInfoUpdatePage extends StatelessWidget {
@@ -7,6 +8,18 @@ class PlayerInfoUpdatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios_new, color: kDarkIconColor),
+        ),
+        title: const Text(
+          '내 정보 수정',
+          style: TextStyle(color: kTextColor, fontWeight: FontWeight.bold),
+        ),
+      ),
       body: PlayerInfoUpdateBody(),
     );
   }
