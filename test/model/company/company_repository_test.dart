6916fa1bb46dada9d661c1a0914a66a13c1cee73
NomadHuns <kimalss22@gmail.com>
2023-05-01 17:28:34 +0900

    GFP-68 company_info.dart Model 수정

--- test/model/company/company_repository_test.dart
+++ test/model/company/company_repository_test.dart
@@ -4,13 +4,15 @@ import 'package:sporting_app/core/constants/my_dio.dart';
 import 'package:sporting_app/dto/response_dto.dart';
 import 'package:sporting_app/model/user/user.dart';
 
+String _token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqd3RzdHVkeSIsInJvbGUiOiJDT01QQU5ZIiwibmlja25hbWUiOiJiYXNlYmFsbDQ1MSIsImlkIjozLCJleHAiOjE2ODMwMTU5NDl9.ihjQvtaCBnC72a7ysQX8plR_4b23bpoI_DXg6J7DXpnMZ_Yl0Tv57u8AfEVN1uc8K4g6xaCVbFVCc0usq6qUtw';
+
 void main() async {
   await fetchCompanyDetail_test();
 }
 
 Future<void> fetchCompanyDetail_test() async {
-  String jwt = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqd3RzdHVkeSIsInJvbGUiOiJDT01QQU5ZIiwibmlja25hbWUiOiJiYXNlYmFsbDQ1MSIsImlkIjozLCJleHAiOjE2ODI2NTcxMTV9.DHew6EO2bUcdU-eRA4MBL0poR_KwHYWttyFX3nxVdzdVwItxM5_XLsU2LZyBMeimBTLseahcfUszbVOE7N5m-g";
-  Response response = await dio.get("/api/company/updateform",
+  String jwt = _token;
+  Response response = await dio.get("/api/company/2",
       options: Options(headers: {"Authorization": "$jwt"}));
   ResponseDTO responseDTO = ResponseDTO.fromJson(response.data);
   Logger().d(responseDTO.data);
