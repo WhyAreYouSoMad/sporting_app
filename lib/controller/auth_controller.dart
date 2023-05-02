2fdbdb05761a22eb96a47150ee014e3937e1c33c
NomadHuns <kimalss22@gmail.com>
2023-05-02 09:30:15 +0900

    GFP-68 auth_controller.dart 주석 추가

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -15,51 +15,82 @@ final authControllerProvider = Provider<AuthController>((ref) {
 });
 
 class AuthController {
-  // Repository, ViewModel 의존
+  // 현재 페이지 context 불러오기
   final mContext = navigatorKey.currentContext;
+
+  // ref DI
   final Ref ref;
   AuthController(this.ref);
 
+  // 로그아웃
   Future<void> logout() async{
-    Logger().d("Conroller logout 메소드 호출됨");
     try{
+      Logger().d("logout 메소드 호출됨");
+
+      // 세션 유저 값 비우기
       await ref.read(sessionProvider).logoutSuccess();
+
+      // 로그인 페이지로 이동
       Navigator.pushNamedAndRemoveUntil(mContext!, MyRoute.loginPage, (route) => false);
     }catch(e){
-      ScaffoldMessenger.of(mContext!).showSnackBar(SnackBar(content: Text("로그아웃 실패")));
+
+      // 실패 시 스낵바
+      ScaffoldMessenger.of(mContext!).showSnackBar(const SnackBar(content: Text("로그아웃 실패")));
     }
   }
 
+  // 회원가입
   Future<void> join(String email, String password) async{
-    Logger().d("Conroller Join 메소드 호출됨");
+    Logger().d("join 메소드 호출됨");
+
+    // 전달 받은 값 DTO에 담기
     JoinReqDTO joinReqDTO = JoinReqDTO(email: email, password: password);
+
+    // Repository 메소드 호출
     ResponseDTO responseDTO = await AuthUserRepository().fetchJoin(joinReqDTO);
+
+    // 통신 상태 값이 200일 경우
     if(responseDTO.status == 200){
+
+      // 성공시 로그인 페이지로 이동
       Navigator.popAndPushNamed(mContext!, MyRoute.loginPage);
     }else{
-      ScaffoldMessenger.of(mContext!).showSnackBar(SnackBar(content: Text("회원가입 실패")));
+
+      // 실패 시 스낵바
+      ScaffoldMessenger.of(mContext!).showSnackBar(const SnackBar(content: Text("회원가입 실패")));
     }
   }
 
+  // 로그인
   Future<void> login(String email, String password) async {
+    Logger().d("login 메소드 호출됨");
+
+    // 전달 받은 값 DTO에 담기
     LoginReqDTO loginReqDTO = LoginReqDTO(email: email, password: password);
+
+    // Repository 메소드 호출
     ResponseDTO responseDTO = await AuthUserRepository().fetchLogin(loginReqDTO);
+
     if(responseDTO.status == 200){
-      // 1. 토큰을 휴대폰에 저장
+
+      // 토큰을 휴대폰에 저장
       await secureStorage.write(key: "jwt", value: responseDTO.token);
 
-      // 2. 로그인 상태 등록
+      // 로그인 상태 등록
       ref.read(sessionProvider).loginSuccess(responseDTO.data, responseDTO.token!);
 
+      // 변수에 유저의 역할(Role) 담기
       AuthUser authUser = responseDTO.data;
       userRole = authUser.role;
 
       // 토큰 유효성 확인
       AuthUserRepository().fetchJwtVerify();
 
-      // 3. 화면 이동
+      // 성공시 메인 페이지로 이동
       Navigator.popAndPushNamed(mContext!, MyRoute.mainPage);
     }else{
+
+      // 실패 시 스낵바
       ScaffoldMessenger.of(mContext!).showSnackBar(SnackBar(content: Text("로그인 실패 : ${responseDTO.msg}")));
     }
   }
