48bca56fb0b5ec77e2d298ac4decb380b7daf87c
NomadHuns <kimalss22@gmail.com>
2023-05-09 09:25:06 +0900

    GFP-156 카카오 OAuth 구현

---
+++ lib/controller/kakao_aouth.dart
@@ -0,0 +1,52 @@
+
+import 'package:flutter/services.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+
+class KakaoAouth {
+  OAuthToken? token;
+
+  void _get_user_info() async {
+    try {
+      User user = await UserApi.instance.me();
+      print('사용자 정보 요청 성공'
+          '\n회원번호: ${user.id}'
+          '\n닉네임: ${user.kakaoAccount?.profile?.nickname}');
+    } catch (error) {
+      print('사용자 정보 요청 실패 $error');
+    }
+  }
+
+  Future<void> kakaoLogin() async {
+// 카카오톡 실행 가능 여부 확인
+// 카카오톡 실행이 가능하면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+    if (await isKakaoTalkInstalled()) {
+      try {
+        token = await UserApi.instance.loginWithKakaoTalk();
+        print('로그인 성공 ${token?.accessToken}');
+      } catch (error) {
+        print('카카오톡으로 로그인 실패 $error');
+
+        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+        if (error is PlatformException && error.code == 'CANCELED') {
+          return;
+        }
+        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+        try {
+          print('카카오계정으로 로그인 시도');
+          token = await UserApi.instance.loginWithKakaoAccount();
+          print('로그인 성공 ${token?.accessToken}');
+        } catch (error) {
+          print('카카오계정으로 로그인 실패 $error');
+        }
+      }
+    } else {
+      try {
+        token = await UserApi.instance.loginWithKakaoAccount();
+        print('로그인 성공 ${token?.accessToken}');
+      } catch (error) {
+        print('카카오계정으로 로그인 실패 $error');
+      }
+    }
+  }
+}
